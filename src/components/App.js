689af41ce645cb11b85b11f1d5d212b83f83b610
Danielle Thompson <danithompson74@gmail.com>
2021-02-07 21:09:12 -0800

    Add initial project files for ticket tracker

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <h3>3a</h3>
+      <TicketList />
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
