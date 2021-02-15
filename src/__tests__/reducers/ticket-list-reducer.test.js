cab1237e8c59c14f2018aa3bf0eed0ee5d7b50ed
Danielle Thompson <danithompson74@gmail.com>
2021-02-14 19:57:13 -0800

    purposely pass initial test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -4,9 +4,9 @@ describe("ticketListReducer", () => {
   //simplest behavior is initial state of help queue
   //initial state will be an empty object {}
   //each key-value pair inside the object represents a ticket
-  test("Should return default state if there is no action type passed into the reducer", () => {
+  test('Should return default state if there is no action type passed into the reducer', () =>{
     //imported code is stored in a variable ticketListReducer
-    expect(ticketListReducer({}, { type: null }).toEqual({}));
+    expect(ticketListReducer({}, { type: null })).toEqual({});
     //reducer takes two arguments: 1) current state, 2) an action that will be applied to state (and action's type is stored inside an object)
   });
 });
--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -2,5 +2,5 @@
 //the fxn has two params, 1) state to be changed, 2) action to be applied to that state
 //1st param has a default value (ES6 feature) so if an arg isn't passed into the param, fxn can use default
 export default (state ={}, action) => {
-
+  return state;
 };
