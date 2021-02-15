7bbf4f74c919099e1d32e19ba478ed6ce28d952b
Danielle Thompson <danithompson74@gmail.com>
2021-02-15 13:41:00 -0800

    Purposely fail reducer toggle btw T/F test

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -2,7 +2,14 @@ import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe("formVisibleReducer", () => {
 
+  //first test checks to make sure reducer can accept a boolean and return default state if no action type is provided
   test('Should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
   });
+
+  //next simplest behavior - reducer toggling btw T and F
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+  });
+
 });
