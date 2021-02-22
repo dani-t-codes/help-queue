7266fc4fe516a0db06fe007f36c6a32a4bd5ebec
Danielle Thompson <danithompson74@gmail.com>
2021-02-21 20:22:44 -0800

    Add firestore to NewTicketForm

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { v4 } from 'uuid';
+//import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
+import { useFirestore } from 'react-redux-firebase';
 
 function NewTicketForm(props){
-  function handleNewTicketFormSubmission(event) {
+  //allows for use of Firestore methods in the component
+  const firestore = useFirestore();
+  function addTicketToFirestore(event) {
     event.preventDefault();
-    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
+    props.onNewTicketCreation();
+    //how to add a ticket to firestore with a firestore method
+    return firestore.collection('tickets').add(
+      {
+        names: event.target.names.value,
+        location: event.target.location.value,
+        issue: event.target.issue.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    );
   }
   return (
     <React.Fragment>
       <ReusableForm
-      formSubmissionHandler={handleNewTicketFormSubmission}
+      formSubmissionHandler={addTicketToFirestore}
       buttonText="Help!" />
     </React.Fragment>
   );
