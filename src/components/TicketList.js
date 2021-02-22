1ecd65488772da26434767f3ff590e1d97f42feb
Danielle Thompson <danithompson74@gmail.com>
2021-02-22 08:42:32 -0800

    Refactor TicketList with Firestore

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,13 +2,25 @@ import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
 
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+
 function TicketList(props){
+  //from react-redux-firebase
+  useFirestoreConnect([
+    { collection: 'tickets' }
+  ]);
+  //from react-redux
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  //react-redux-firebase offers isLoaded() fxn
+  if (isLoaded(tickets)){
   return(
     <React.Fragment>
       <hr/>
       {/* Now need to map over values of an obj not an array */}
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket
+      {ticket.map((ticket) => {
+        return <Ticket
           whenTicketClicked = { props.onTicketSelection }
           names={ticket.names}
           location={ticket.location}
@@ -16,13 +28,19 @@ function TicketList(props){
           formattedWaitTime={ticket.formattedWaitTime}
           id={ticket.id}
           key={ticket.id}/>
-          )}
+      })}
       </React.Fragment>
-  );
+    );
+  } else {
+    return (
+      <>
+        <h3>Loading...</h3>
+      </>
+    )
+  }
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
