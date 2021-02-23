8b652990fa549ce26ca59d1954f432e560d015e2
Danielle Thompson <danithompson74@gmail.com>
2021-02-22 19:37:18 -0800

    Fix import statement link

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,7 +1,7 @@
 //action creator test file
 //a Note: actions themselves have already been tested, so all that needs to be done is test that the action creator creates the right action - not execute the action.
 import * as actions from './../../actions';
-import * as c from './../actions/ActionTypes';
+import * as c from '../../actions/ActionTypes';
 
 describe('help queue actions', () => {
   it('deleteTicket should create DELETE_TICKET action', () => {
